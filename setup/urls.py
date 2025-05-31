606d287581f602b03a422ca92fbcb3002560a486
Sarah Barros <sarahgbarros@hotmail.com>
2025-05-31 11:49:12 -0400

    Jazzmin e endpoint list

--- setup/urls.py
+++ setup/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from apps.Users.views import RegisterUserView, CreateSuperUserAPIView, LoginView, LogoutView
-from apps.MediaList.views import SaveMedia, SearchMediaByActors, SearchMediaByTitle
+from apps.MediaList.views import SaveMedia, SearchMediaByTitle
 from apps.Reviews.views import CreateReview, DeleteReview
 
 urlpatterns = [
@@ -11,7 +11,6 @@ urlpatterns = [
     path('api/register_user/', RegisterUserView.as_view(),name='api/register_user'),
     path('api/register_superuser/', CreateSuperUserAPIView.as_view(),name='api/register_superuser'),
     path('api/save_media/', SaveMedia.as_view(),name='api/save_media'),
-    path('api/search_by_actor/', SearchMediaByActors.as_view(),name='api/search_by_actor'),
     path('api/search_by_title/', SearchMediaByTitle.as_view(),name='api/search_by_title'),
     path('api/create_review/', CreateReview.as_view(),name='api/createreview'),
     path('api/delete_review/', DeleteReview.as_view(),name='api/deletereview'),
